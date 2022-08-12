f2b52149b686ec78860152d00aaa770f809aafc3
taehoon7289 <taehoon7289@gmail.com>
2022-08-12 17:02:31 +0900

    feat: scss 적용
    - scss 적용

--- js/main.js
+++ js/main.js
@@ -1,10 +1,1 @@
-import {range} from 'lodash'
-
-const test = () => {
-  const list = range(0, 10000, 1)
-  list.forEach(item => console.log(item))
-  document.getElementById("temp").append('ddddddddddddd')
-  console.log(document.getElementById("temp"))
-}
-
-test()
+import '/css/scss/layout.scss'
