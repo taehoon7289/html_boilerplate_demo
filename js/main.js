012a0ca814fc24d53c6321b882e3fe24993993b7
taehoon7289 <taehoon7289@gmail.com>
2022-08-18 17:00:28 +0900

    feat: draggable, dropzone 추가
    - draggable, dropzone 추가

--- js/main.js
+++ js/main.js
@@ -1,1 +1,36 @@
 import '/css/scss/layout.scss'
+import '/css/scss/bg-color.scss'
+import '/css/scss/event.scss'
+
+function injectDraggableEvent() {
+  const draggableEls = document.querySelectorAll(".draggable")
+  const dropzoneEls = document.querySelectorAll(".dropzone")
+
+  for (const el of draggableEls) {
+    el.addEventListener('dragstart', event => {
+      const target = event.target
+      target.classList.add('dragging')
+    })
+
+    el.addEventListener('dragend', event => {
+      const target = event.target
+      target.classList.remove('dragging')
+    })
+
+  }
+
+  // dropzone element
+  for (const el of dropzoneEls) {
+    el.addEventListener('dragenter', event => {
+      const target = event.target
+      target.classList.add('dragover')
+    })
+    el.addEventListener('dragleave', event => {
+      const target = event.target
+      target.classList.remove('dragover')
+    })
+  }
+
+}
+
+injectDraggableEvent()
